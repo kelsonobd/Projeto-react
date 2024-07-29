ffeb7380967b02054581aeb8c5ff9498cf52e280
Kelsonobd <kelsonobd@gmail.com>
2024-07-29 09:38:54 -0300

    state lift

--- meu-projeto-react/src/App.js
+++ meu-projeto-react/src/App.js
@@ -1,20 +1,16 @@
+import { useState } from 'react';
 import './App.css';
-import Condicional from './components/Condicional';
-import OutraLista from './components/OutraLista';
-
-
+import SeuNome from './components/SeuNome';
+import Saudacao from './components/Saudacao';
 function App() {
+  const [nome, setNome] = useState()
 
- const meusItens = ['React', 'Vue', 'Angular']
 
   return (
     <div className="App">
-
-   <h1>Redenreização de Listas</h1>
-   <OutraLista itens ={meusItens} />
-   <OutraLista itens ={[]} />
-   
-
+      <h1>State Lift</h1>
+      <SeuNome setNome={setNome}/>
+      <Saudacao nome={nome} />
     </div>
 
   );
