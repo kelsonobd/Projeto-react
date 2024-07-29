14fa0049b17935637f0bcd00f22364783fab27c3
Kelsonobd <kelsonobd@gmail.com>
2024-07-29 11:04:04 -0300

    router route routes & link

--- meu-projeto-react/src/App.js
+++ meu-projeto-react/src/App.js
@@ -1,19 +1,23 @@
-import { useState } from 'react';
-import './App.css';
-import SeuNome from './components/SeuNome';
-import Saudacao from './components/Saudacao';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Contato from "./pages/Contato";
+import Empresa from "./pages/Empresa";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+
 function App() {
-  const [nome, setNome] = useState()
-
-
   return (
-    <div className="App">
-      <h1>State Lift</h1>
-      <SeuNome setNome={setNome}/>
-      <Saudacao nome={nome} />
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/empresa" element={<Empresa />} />
+        <Route exact path="/contato" element={<Contato />} />
+      </Routes>
+      <Footer/>
+    </Router>
 
-  );
+  )
 }
 
 export default App;
